b05314f61e941015effb6039615610c849d8c793
Miguel Fletes <mfletes@csumb.edu>
2018-02-01 01:26:28 +0000

    add all lab1 files

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8/">
+        <title>Miguel Fletes: Personal Website</title>
+        
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        
+        <header>
+            <h1>
+                Miguel Fletes
+            </h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+            
+            <a href="index.html" id="home">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <main>
+            <figure id="me">
+                <img src="img/IMG_0719.jpg">
+            </figure>
+            <div id="welcomeText">
+                <br/><br/>
+                Hello!<br/>
+                <p>
+                    Thank you for visiting my proffesional portfolio website.
+                </p>
+                <p>
+                    I am a software Engineering student at California State University Monterey Bay.
+                </p>
+                <p>
+                    Feel free to contact me.
+                </p>
+                <em>
+                    <!--"With ordinary talent and extraordinary <strong>perserverence</strong>, all things are attainable."-->
+                    "Do not believe everything you read online"
+                </em>
+                <br/>
+                 <!--Thomas F. Buxton-->
+                 -Abraham Lincoln
+            </div>
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            Internet Programming, 2018 <br/>
+            <img src="https://breezymartin.files.wordpress.com/2013/10/csumb_logo.jpg">
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
